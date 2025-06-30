5f8540cf19afb7d4c8c39757e16a86d84ca887e9
joeDougan <joedougan00@gmail.com>
2025-06-30 14:07:52 +0100

    made Quotes.java for writing and running Java challenge 2

---
+++ bites/examples/Quotes.java
@@ -0,0 +1,26 @@
+class Quotes{
+
+    public static void kermitTheFrog(){
+        System.out.println("It isn't easy being green, but its beautiful");
+    }
+    public static void messi(){
+        System.out.println("Baaaaa");
+    }
+    public static void daizenMaeda(){
+        System.out.println("I shoot i score, wahey!!");
+    }
+
+    public static void calTheDragon(){
+        System.out.println("dont ever believe you're good enough");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Quotes.kermitTheFrog();
+        Quotes.messi();
+        Quotes.daizenMaeda();
+        Quotes.calTheDragon();
+    }
+}
