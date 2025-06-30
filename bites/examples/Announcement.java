d466245578d63bbda8e28329e0fb04b1971865bd
joeDougan <joedougan00@gmail.com>
2025-06-30 14:17:40 +0100

    Changed from static to instance methods

--- bites/examples/Announcement.java
+++ bites/examples/Announcement.java
@@ -1,35 +1,38 @@
 class Announcement{
 
-    public static void alert(String message) {
+    public static void main(String[] args) {
+        Announcement myAnnouncement = new Announcement();
+        myAnnouncement.alert("attention please!");
+        myAnnouncement.apology();
+        myAnnouncement.changeOfPlan();
+        myAnnouncement.politeNotice();
+        myAnnouncement.helpNeeded();
+        myAnnouncement.celebration();
+    }
+
+    public void alert(String message) {
         System.out.println(message);
     }
 
-    public static void changeOfPlan() {
+    public void changeOfPlan() {
         System.out.println("Whoops you cant od this anymore time to change the plan");
     }
 
-    public static void apology(){
+    public void apology(){
         System.out.println("Sorry for any inconvenience caused");
     }
 
-    public static void politeNotice(){
+    public void politeNotice(){
         System.out.println("Please remember to take your belongings with you");
     }
 
-    public static void helpNeeded(){
+    public void helpNeeded(){
         System.out.println("If help is needed please ask a memeber of staff");
     }
 
-    public static void celebration(){
+    public void celebration(){
         System.out.println("Congrats you're onto week 7 of the bootcamp");
     }
 
-    public static void main(String[] args) {
-        Announcement.alert("This is a message from the alert function");
-        Announcement.apology();
-        Announcement.politeNotice();
-        Announcement.helpNeeded();
-        Announcement.celebration();
-        Announcement myAnnouncement
-    }
+
 }
