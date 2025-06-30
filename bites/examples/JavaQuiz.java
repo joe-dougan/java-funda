19a52a91d0430b082acaf8a5d699a19c63693016
joeDougan <joedougan00@gmail.com>
2025-06-30 11:30:04 +0100

    First Commit

---
+++ bites/examples/JavaQuiz.java
@@ -0,0 +1,10 @@
+package bites.examples;
+
+public class JavaQuiz {
+    public static void main(String[] args) {
+        System.out.println("All java code has to be inside a class");
+        System.out.println("translating source code from high level language to low level language");
+        System.out.println("it is called automatically when the java file runs. your file must have a main method in order to compile");
+        System.out.println("void means None in python, this means we are not returning anything. But if we used String, Int instead of void wed be returning the relevant data type");
+    }
+}
---
+++ bites/examples/Message.java
@@ -0,0 +1,8 @@
+package bites.examples;
+
+class Message {
+    public static void main(String[] args) {
+        System.out.println("Hello? Is there anyone out there?");
+    }
+}
+
